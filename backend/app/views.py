80be34b4c0fd63fe97a129cc410f077e9d36f847
Marcela <arqmrcl@gmail.com>
2022-10-09 14:41:07 -0300

    desenvolvendo API com Python e Django

---
+++ backend/app/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return Response({"nome": "Marcela Machado", "idade": 25}, status= 200)
---
+++ backend/teacher/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from teacher.models import Professor, Aula
+from django.forms import ValidationError
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
+
+class CadastrarAulaSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    nome = serializers.CharField(max_length=100)
+
+    def validate_nome(self, value):
+        if len(value) < 3:
+            raise ValidationError("deve ter pelo menos 3 caracteres")
+        return value
+
+class AulaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aula
+        fields = '__all__'
